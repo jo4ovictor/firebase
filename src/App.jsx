729a3c8c75f870042174c8a369a3669de32dd093
jo4ovictor <joaovictordeaguiarmoura@gmail.com>
2022-11-16 21:13:19 -0400

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,12 @@
-import { async } from '@firebase/util';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore';
-import {useState} from 'react'
-import {db} from './firebaseConnection'
+import RoutesApp from './routes';
+import {BrowserRouter} from 'react-router-dom';
+import './app.css';
 function App() {
-
-      const[titulo, setTitulo] = useState('');
-      const[autor, setAutor] = useState('');
-      const [posts, setPosts] = useState([]);
-      const [idPost, setIdPost] = useState('');
-      async function handleAdd() {
-        await addDoc(collection(db, "posts", "12345"), {
-          titulo: titulo,
-          autor: autor,
-        })
-        .then(() => {
-          console.log("Dados gravados no banco!")
-          setAutor('');
-          setTitulo('');
-        })
-        .catch((error) => {
-          console.log("Gerou Erro" + error)
-        })
-      }
-      async function buscarPost() {
-        const postsRef = collection(db, "posts")
-        await getDocs(postsRef)
-        .then((snapshat) => {
-          let lista = [];
-          snapshat.forEach((doc) => {
-            lista.push({
-              id: doc.id,
-              titulo: doc.data().titulo,
-              autor: doc.data().autor,
-            })
-          })
-          setPosts(lista);
-        })
-        .catch((error) => {
-          console.log("Deu algum Erro ao buscar")
-        })
-      }
-      async function editarPost(){
-        const docRef = doc(db, "posts", idPost)
-        await updateDoc(docRef, {
-          titulo: titulo,
-          autor: autor
-        })
-        .then(() => {
-          console.log("Atualizado com sucesso")
-          setIdPost('')
-          setTitulo('')
-          setAutor('')
-        })
-        .catch(() => {
-          console.log("Erro ao atualizar banco")
-        })
-      }
-      async function excluirPost(id){
-        const docRef = doc(db, 'posts', id)
-        await deleteDoc(docRef)
-        .then(()=>{
-          alert("post deletado com sucesso")
-        })
-        .catch((error)=>{
-          alert("Erro"+error)
-        })
-      }
   return (
-    <div className="container">
-          <label>IdPost</label>
-          <input type="text"
-          placeholder="informe o id"
-          value={idPost}
-          onChange={(e) => setIdPost(e.target.value)}/> <br/>
-         
-         <label> Titulo: </label>
-         <textarea type="text" placeholder='Digite o titulo'
-         value={titulo} onChange= {(e) => setTitulo(e.target.value)}>
-         </textarea>
-         <label> Autor: </label>
-         <input type="text" 
-         placeholder='Digite o autor do post'
-         value={autor} onChange= {(e) => setAutor(e.target.value)}>
-          </input>
-         
-          <ul>
-            {posts.map( (post) => {
-              return(
-                <li key={post.id}>
-                  <strong>Id Post: {post.id}</strong> <br/>
-                  <span>Titulo: {post.titulo}</span> <br/>
-                  <span>Autor: {post.autor}</span> <br/>
-                  <button onClick={()=>excluirPost(post.id)}> Excluir </button> <br/>
-                </li>
-              )
-            })}
-          </ul>
-          <button onClick={handleAdd}> Cadastrar </button>
-         <button onClick={buscarPost}> Buscar Post</button>
-          <button onClick={editarPost}>Editar Post</button>
-    </div>
-  );
-  
-}
+    <BrowserRouter>
+      <RoutesApp/>
+    </BrowserRouter>    
+  )
+}//fim da função App
 export default App;
+
--- src/firebaseConnection.jsx
+++ src/firebaseConnection.jsx
@@ -1,6 +1,8 @@
-import {initializeApp} from 'firebase/app';
-import {getFirestore} from 'firebase/firestore';
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
 
+//config colocar
 const firebaseConfig = {
   apiKey: "AIzaSyBVLx9xpOqPIWs-lEjMpIutesKSNcP1crU",
   authDomain: "aula2610-ab5d1.firebaseapp.com",
@@ -9,7 +11,8 @@ const firebaseConfig = {
   messagingSenderId: "509993266900",
   appId: "1:509993266900:web:43702a67c91dd8d87f1541"
 };
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-export {db};
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
+  export {db, auth};
